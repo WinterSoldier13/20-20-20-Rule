a321bb708ad17ce0981d577b55a01908498e03f6
WinterSoldier <ayushsingh1315@gmail.com>
2019-12-12 14:53:59 +0530

    Update rule.py

--- rule.py
+++ rule.py
@@ -3,7 +3,8 @@ import time
 def display():
     time.sleep(1200)
     n = toast()
-    n.show_toast("WARNING","It has been 20 mins since you last took a break, now look 20ft. away for 20seconds :)",duration=12)
+    #download any suitable icon of your choice and rename it icon.ico
+    n.show_toast("WARNING","It has been 20 mins since you last took a break, now look 20ft. away for 20seconds :)",icon='icon.ico',duration=12)
     display()
 
 if __name__ == '__main__':
